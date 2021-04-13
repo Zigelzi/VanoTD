a852a9e52fdbb1305e84960bf5c692d024c0776a
Miika Savela <miika.a.savela@gmail.com>
2021-04-13 18:23:03 +0300

    Add TMP and TileLabels. Make all road tiles as prefab variants from basic grass tile

---
+++ Assets/Environment/Tiles/TileLabel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[ExecuteAlways]
+public class TileLabel : MonoBehaviour
+{
+    [SerializeField] Color defaultColor = Color.white;
+
+    TextMeshPro tileLabel;
+    Vector2Int coordinates = new Vector2Int();
+    // Start is called before the first frame update
+    void Awake()
+    {
+        tileLabel = GetComponentInChildren<TextMeshPro>();
+        tileLabel.enabled = true;
+        SetTileText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Application.isPlaying)
+        {
+            SetTileText();
+            SetGameObjectName();
+        }
+    }
+
+    void SetTileText()
+    {
+        float gridSize = 10;
+        coordinates.x = Mathf.RoundToInt(transform.position.x / gridSize);
+        coordinates.y = Mathf.RoundToInt(transform.position.z / gridSize);
+        tileLabel.text = $"{coordinates.x}, {coordinates.y}";
+    }
+
+    void SetGameObjectName()
+    {
+        gameObject.transform.name = $"Tile_{coordinates.x}_{coordinates.y}";
+    }
+}
