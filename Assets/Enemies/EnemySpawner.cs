7697445afa4063a84336cdbf2e0111768d5bc032
Miika Savela <miika.a.savela@gmail.com>
2021-04-13 18:38:49 +0300

    Add EnemySpawner and placeholder enemy

---
+++ Assets/Enemies/EnemySpawner.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] [Range(0, 10)] int poolSize = 5;
+    [SerializeField] [Range(0.5f, 30f)] float spawnInterval = 3f;
+    [SerializeField] bool spawningEnabled = true;
+
+    GameObject[] objectPool;
+    Vector3 spawnPosition;
+
+    void Awake()
+    {
+        SetSpawnPosition();
+        PopulatePool();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetSpawnPosition()
+    {
+        // Update this to use the grid manager when it's implemented
+        spawnPosition = transform.position;
+    }
+
+    void PopulatePool()
+    {
+        objectPool = new GameObject[poolSize];
+        for (int i = 0; i < objectPool.Length; i++)
+        {
+            objectPool[i] = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity, transform);
+            objectPool[i].SetActive(false);
+        }
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        while (spawningEnabled)
+        {
+            for (int i = 0; i < objectPool.Length; i++)
+            {
+                SpawnEnemy(i);
+                yield return new WaitForSeconds(spawnInterval);
+            }
+        }
+    }
+
+    void SpawnEnemy(int poolPosition)
+    {
+        if (enemyPrefab != null && objectPool.Length > 0)
+        {
+            objectPool[poolPosition].SetActive(true);
+        }
+    }
+}
