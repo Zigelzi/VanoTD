a6c0d14a6f624f7e5039b9521d8faa9d6fd83729
Miika Savela <miika.a.savela@gmail.com>
2021-04-23 19:12:42 +0300

    Start Tower and Tile implementation

---
+++ Assets/Environment/Tiles/Tile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlaceTower();
+    }
+
+    bool PlaceTower()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log($"You clicked to tile: {transform.position}");
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+}
---
+++ Assets/Towers/Tower.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] int buildCost = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Build()
+    {
+        return;
+    }
+}
